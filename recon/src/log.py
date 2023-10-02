fd499fc8da8a07937cba5768ca3b12ebce761266
Gabriel Blank Stift Mousquer <gabrielblanksm@gmail.com>
2023-10-02 04:39:08 -0300

    add log shipper and log ingestion tool

--- recon/src/log.py
+++ recon/src/log.py
@@ -1,13 +1,24 @@
 import logging
-import sys
+import os
 
 
 LOGGER = logging.getLogger(__name__)
 
 
 def configure_log():
-    logging.basicConfig(
-        format='[%(asctime)s] [%(levelname)s] [%(filename)s] %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        level=logging.DEBUG,
-        stream=sys.stderr)
+    formatter = logging.Formatter(
+        fmt='[%(asctime)s] [%(levelname)s] [%(filename)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.DEBUG)
+
+    os.makedirs('/logs', exist_ok=True)
+    file_handler = logging.FileHandler('/logs/recon.log', mode='w')
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+
+    LOGGER.setLevel(logging.DEBUG)
+    LOGGER.addHandler(console_handler)
+    LOGGER.addHandler(file_handler)
