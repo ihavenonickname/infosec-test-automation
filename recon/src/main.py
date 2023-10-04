278cb50e7e0768bf66d3b11d4fe822b770f8c550
Gabriel Blank Stift Mousquer <gabrielblanksm@gmail.com>
2023-10-04 17:41:34 -0300

    Fix abrupt shutdowns
    Now recon service honors sigint and sigterm.
    
    Fix #6

--- recon/src/main.py
+++ recon/src/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import signal
 import uuid
 
 import aiomqtt
@@ -11,16 +12,7 @@ import subdomain_enumeration
 import dns_vuln_scan
 
 
-async def main():
-    configure_log()
-
-    LOGGER.debug('MQTT_HOST: %s', MQTT_HOST)
-    LOGGER.debug('MQTT_PORT: %d', MQTT_PORT)
-
-    asyncio.create_task(subdomain_enumeration.run_main_loop())
-    asyncio.create_task(subdomains_info_gathering.run_main_loop())
-    asyncio.create_task(dns_vuln_scan.run_main_loop())
-
+async def watch_start_messages():
     LOGGER.debug('Starting main run')
 
     async with aiomqtt.Client(MQTT_HOST, MQTT_PORT) as client:
@@ -42,6 +34,46 @@ async def main():
                 await client.publish(ReconTopics.SUBDOMAIN_ENUMERATION, payload)
                 await client.publish(ReconTopics.DNS_VULN_SCAN, payload)
                 await client.publish('webapp/start', payload)
+
+
+async def main():
+    configure_log()
+
+    LOGGER.debug('MQTT_HOST: %s', MQTT_HOST)
+    LOGGER.debug('MQTT_PORT: %d', MQTT_PORT)
+
+    cancellation_event = asyncio.Event()
+
+    def cancellation_handler(signum, frame):
+        LOGGER.info('Handling signum %d', signum)
+        cancellation_event.set()
+
+    signal.signal(signal.SIGINT, cancellation_handler)
+    signal.signal(signal.SIGTERM, cancellation_handler)
+
+    tasks = [
+        asyncio.create_task(subdomain_enumeration.run_main_loop()),
+        asyncio.create_task(subdomains_info_gathering.run_main_loop()),
+        asyncio.create_task(dns_vuln_scan.run_main_loop()),
+        asyncio.create_task(watch_start_messages()),
+    ]
+
+    await cancellation_event.wait()
+
+    LOGGER.debug('Cancelling tasks')
+
+    for task in tasks:
+        task.cancel()
+
+    LOGGER.debug('Waiting for tasks to finish')
+
+    for task in tasks:
+        try:
+            await task
+        except asyncio.CancelledError:
+            pass
+
+    LOGGER.debug('Bye')
 
 if __name__ == '__main__':
     asyncio.run(main())
