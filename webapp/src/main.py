4bf7ecbd59731c5ea5410ec1212dbb8b72536875
Gabriel <ihavenonickname@users.noreply.github.com>
2023-10-10 19:45:45 -0300

    crud for open observe users management  (#18)
    Open Observe provides a quite limited API.
    
    - List users: https://openobserve.ai/docs/api/user/list/
    - Create user: https://openobserve.ai/docs/api/user/create/
    - Remove user: https://openobserve.ai/docs/api/user/delete/

--- webapp/src/main.py
+++ webapp/src/main.py
@@ -3,24 +3,30 @@ import base64
 import contextlib
 import os
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 import uvicorn
 import validators
 
 from database import Database
 from messaging import MessagingApi
+from open_observe_api import OpenObserveApi
 
 
 MQTT_HOST = os.environ['MQTT_HOST']
 MQTT_PORT = os.environ['MQTT_PORT']
 DATA_DIR = os.environ['DATA_DIR']
 OPEN_OBSERVE_BASE_URL = os.environ['OPEN_OBSERVE_BASE_URL']
+OPEN_OBSERVE_ROOT_EMAIL = os.environ['OPEN_OBSERVE_ROOT_EMAIL']
+OPEN_OBSERVE_ROOT_PASSWORD = os.environ['OPEN_OBSERVE_ROOT_PASSWORD']
 
 DATABASE = Database(os.path.join(DATA_DIR, 'webapp.db'))
 MESSAGING_API = MessagingApi(MQTT_HOST, int(MQTT_PORT))
+OPEN_OBSERVE_API = OpenObserveApi(
+    OPEN_OBSERVE_BASE_URL, OPEN_OBSERVE_ROOT_EMAIL, OPEN_OBSERVE_ROOT_PASSWORD)
 
 
 @contextlib.asynccontextmanager
@@ -47,15 +53,66 @@ app.mount(
     StaticFiles(directory='./static'),
     'static')
 
+templates = Jinja2Templates(directory='./src/templates')
+
 
 class StartReconPipelineModel(BaseModel):
     domain: str
 
 
+class CreateUserModel(BaseModel):
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+
+
 @app.get('/', response_class=HTMLResponse)
-def get_index():
-    with open('./src/pages/index.html') as f:
-        return f.read()
+def get_index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.get('/users', response_class=HTMLResponse)
+def get_users(request: Request):
+    ok, data = OPEN_OBSERVE_API.list_users()
+
+    if not ok:
+        raise HTTPException(status_code=500, detail='Could not list users')
+
+    return templates.TemplateResponse('users.html', {
+        'request': request,
+        'users': data['data'],
+    })
+
+
+@app.get('/users/create', response_class=HTMLResponse)
+def get_users_create(request: Request):
+    return templates.TemplateResponse('users-create.html', {
+        'request': request,
+    })
+
+
+@app.post('/users')
+async def post_users(model: CreateUserModel):
+    ok, data = OPEN_OBSERVE_API.create_user(
+        model.email, model.first_name, model.last_name, model.password)
+
+    if ok:
+        return Response(status_code=204)
+
+    raise HTTPException(
+        status_code=400, detail=data if data else 'Unexpected error')
+
+
+@app.delete('/users/{email}')
+def delete_users(email: str):
+    ok, data = OPEN_OBSERVE_API.delete_user(email)
+
+    if ok:
+        return Response(status_code=204)
+
+    raise HTTPException(
+        status_code=400, detail=data if data else 'Unexpected error')
 
 
 @app.post('/pipelines')
@@ -72,7 +129,8 @@ def get_updates(count: int, last_trace_id: str | None = None):
 
     for execution in executions:
         trace_id = execution['trace_id']
-        query = base64.b64encode(f"trace_id='{trace_id}'".encode('utf8')).decode('ascii')
+        query = base64.b64encode(
+            f"trace_id='{trace_id}'".encode('utf8')).decode('ascii')
         execution['logs_url'] = f'{OPEN_OBSERVE_BASE_URL}/web/logs?stream=default&period=15d&refresh=0&sql_mode=false&query={query}&org_identifier=recon'
 
     return executions
